40f5de01d5b180263a1a00114365ada2c74ee59a
Marcos Castro <mcastrosouza@live.com>
2016-12-17 08:23:23 -0300

    added logic of the game

--- app/src/main/java/com/mcastrosouza/guessthenumber/MainActivity.java
+++ app/src/main/java/com/mcastrosouza/guessthenumber/MainActivity.java
@@ -2,6 +2,11 @@ package com.mcastrosouza.guessthenumber;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.Random;
 
 /**
  * Created by Marcos Castro on 17/12/16.
@@ -9,10 +14,39 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    EditText number;
+    TextView tip, attempts;
+    Random rand;
+    int guess_number, count_attempts, min = 1, max = 100;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // arquivo que está dentro do layout
+        setContentView(R.layout.activity_main);
+
+        rand = new Random();
+        guess_number = rand.nextInt((max - min) + 1) + min;
+        number = (EditText)findViewById(R.id.editTextNumber);
+        tip = (TextView)findViewById(R.id.textViewTip);
+        attempts = (TextView)findViewById(R.id.textViewAttempts);
+    }
+
+    public void checkNumber(View v) {
+
+        int n = Integer.parseInt(number.getText().toString());
+        boolean congratz = false;
+
+        if(n < guess_number)
+            tip.setText("Tip: UP");
+        else if(n > guess_number)
+            tip.setText("Tip: DOWN");
+        else
+        {
+            tip.setText("CONGRATULATIONS!!");
+            congratz = true;
+        }
+
+        count_attempts++;
+        attempts.setText("Attempts: " + Integer.toString(count_attempts));
     }
 }
