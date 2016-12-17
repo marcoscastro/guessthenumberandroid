a93789e48285fd31561369e9d28c8d0ec780edef
Marcos Castro <mcastrosouza@live.com>
2016-12-17 07:08:22 -0300

    added textview, edittext and button

---
+++ app/src/main/java/com/mcastrosouza/guessthenumber/MainActivity.java
@@ -0,0 +1,18 @@
+package com.mcastrosouza.guessthenumber;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Marcos Castro on 17/12/16.
+ */
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // arquivo que está dentro do layout
+    }
+}
